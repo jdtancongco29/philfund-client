c0df1a40b6be9bb18b9628d155dcf2fe3d8dcf58
jdtancongco29 <jdtancongco@gmail.com>
2025-05-18 23:10:00 +0800

    remove unused imports

--- src/pages/MaintenanceAndSecurity/LendingSetup/SchoolOfficeSetup/AddSchoolOfficeDialog.tsx
+++ src/pages/MaintenanceAndSecurity/LendingSetup/SchoolOfficeSetup/AddSchoolOfficeDialog.tsx
@@ -2,7 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"

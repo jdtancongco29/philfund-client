92d903603164d0fbba1c98bd85e5e0722c1d26ca
lovenangelo <lovenangelo.dev@gmail.com>
2025-05-23 22:51:09 +0800

    feat: salary loan

--- src/pages/MaintenanceAndSecurity/LendingSetup/SalaryLoanSetup/page.tsx
+++ src/pages/MaintenanceAndSecurity/LendingSetup/SalaryLoanSetup/page.tsx
@@ -1,5 +1,5 @@
-import MainLayout from '@/components/layout/MainLayout'
-import { SalaryLoanTable } from './SalaryLoanTable'
+import MainLayout from "@/components/layout/MainLayout"
+import { SalaryLoanTable } from "./SalaryLoanTable"
 
 function SalaryLoanSetupPage() {
   return (

b5c716bacccd1ca7f543b273e8a560804d0ede5c
lovenangelo <lovenangelo.dev>
2025-05-29 13:15:54 +0800

    feat: export school feature

--- src/pages/MaintenanceAndSecurity/LendingSetup/SchoolOfficeSetup/Service/SchoolSetupService.ts
+++ src/pages/MaintenanceAndSecurity/LendingSetup/SchoolOfficeSetup/Service/SchoolSetupService.ts
@@ -188,6 +188,44 @@ export const SchoolSetupService = {
       throw new Error(error.response.data.message);
     }
   },
+
+  /**
+   * Export to PDF
+   */
+  exportPdf: async (): Promise<Blob> => {
+    const endpoint = `/borrower/school/export-pdf`;
+    try {
+      const response = await apiRequest<Blob>("get", endpoint, null, {
+        useAuth: true,
+        useBranchId: true,
+        responseType: "blob",
+      });
+      return response.data;
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message || "Failed to export PDF";
+      throw new Error(errorMessage);
+    }
+  },
+
+  /**
+   * Export to CSV
+   */
+  exportCsv: async () => {
+    const endpoint = `/borrower/school/export-csv`;
+    try {
+      const response = await apiRequest("get", endpoint, null, {
+        useAuth: true,
+        useBranchId: true,
+        responseType: "blob",
+      });
+      return response.data;
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message || "Failed to export CSV";
+      throw new Error(errorMessage);
+    }
+  },
 };
 
 export default SchoolSetupService;

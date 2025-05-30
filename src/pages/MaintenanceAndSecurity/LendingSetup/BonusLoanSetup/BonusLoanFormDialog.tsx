dbaf3a1f569e1e6e12d41aefa68c31d4ddbd98f5
lovenangelo <lovenangelo.dev>
2025-05-30 15:08:08 +0800

    fix: input error

--- src/pages/MaintenanceAndSecurity/LendingSetup/BonusLoanSetup/BonusLoanFormDialog.tsx
+++ src/pages/MaintenanceAndSecurity/LendingSetup/BonusLoanSetup/BonusLoanFormDialog.tsx
@@ -546,7 +546,7 @@ export function BonusLoanFormDialog({
                           Maximum Amount
                         </FormLabel>
                         <FormControl>
-                          <Input type="number" step="0.01" placeholder="0.00" {...field} />
+                          <Input type="number" step="0.01" placeholder="0.00" {...field} value={field.value ?? ""} />
                         </FormControl>
                         <FormDescription>Maximum loan amount allowed</FormDescription>
                         <FormMessage />
